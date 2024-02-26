5e2e690b23fc50b2a6a4349807134f1902841929
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-27 00:03:03 +0700

    Add basic_grid activity and layout, add view binding

---
+++ app/src/main/java/exercise/mcommerce/gridview/basicGridview_ex.java
@@ -0,0 +1,14 @@
+package exercise.mcommerce.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basicGridview_ex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_gridview_ex);
+    }
+}
