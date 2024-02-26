318fe73995826e11e7310b134b82047177153a4f
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-27 00:12:03 +0700

    Fea: Load data to basic grid view

--- app/src/main/java/exercise/mcommerce/gridview/basicGridview_ex.java
+++ app/src/main/java/exercise/mcommerce/gridview/basicGridview_ex.java
@@ -3,12 +3,36 @@ package exercise.mcommerce.gridview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import exercise.mcommerce.gridview.databinding.ActivityBasicGridviewExBinding;
 
 public class basicGridview_ex extends AppCompatActivity {
+
+    ActivityBasicGridviewExBinding binding;
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic_gridview_ex);
+        binding = ActivityBasicGridviewExBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadData();
+        addEvents();
+    }
+
+    private void loadData() {
+        String[] data = {"Máy ảnh", "Laptop", "Điện thoại", "Máy tính bảng", "Tai nghe", "Loa", "Ổ cứng di động", "USB", "Thẻ nhớ", "Pin sạc", "Bàn phím", "Chuột"};
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+        binding.gridView.setAdapter(adapter);
+    }
+
+    private void addEvents() {
     }
 }
