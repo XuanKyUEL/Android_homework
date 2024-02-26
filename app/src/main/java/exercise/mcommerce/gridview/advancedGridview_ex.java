d8bf04875852c7d672e6388fe45e3003c09a902f
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-27 01:01:48 +0700

    Fea: Load items to advancedGrid

--- app/src/main/java/exercise/mcommerce/gridview/advancedGridview_ex.java
+++ app/src/main/java/exercise/mcommerce/gridview/advancedGridview_ex.java
@@ -4,11 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import exercise.mcommerce.adapters.BeerAdapter;
+import exercise.mcommerce.gridview.databinding.ActivityAdvancedGridviewExBinding;
+import exercise.mcommerce.models.Beers;
+
 public class advancedGridview_ex extends AppCompatActivity {
+
+    ActivityAdvancedGridviewExBinding binding;
+
+    BeerAdapter adapter;
+
+    ArrayList<Beers> beers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_advanced_gridview_ex);
+        binding = ActivityAdvancedGridviewExBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initData();
+        loadData();
+        addEvents();
+
+    }
+
+    private void initData() {
+        beers = new ArrayList<>();
+        beers.add(new Beers(R.drawable.beer333, "Beer 333", 20000.0));
+        beers.add(new Beers(R.drawable.hanoi, "Beer Hanoi", 25000.0));
+        beers.add(new Beers(R.drawable.saigon, "Beer Saigon", 30000.0));
+        beers.add(new Beers(R.drawable.tiger, "Beer Tiger", 35000.0));
+        beers.add(new Beers(R.drawable.larue, "Beer Larue", 40000.0));
+        beers.add(new Beers(R.drawable.sapporo, "Beer Sapporo", 45000.0));
+    }
+
+    private void loadData() {
+        adapter = new BeerAdapter(
+                advancedGridview_ex.this,
+                R.layout.item_layout,
+                beers
+        );
+        binding.gridViewAdvanced.setAdapter(adapter);
+    }
+
+    private void addEvents() {
     }
 }
