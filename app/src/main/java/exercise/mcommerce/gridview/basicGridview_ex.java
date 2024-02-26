c5f463029dd6a238be3f0ce4767741bc44f4b2a0
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-27 00:18:06 +0700

    Fea: Convert data to modifiedList and add Hold to delete event

--- app/src/main/java/exercise/mcommerce/gridview/basicGridview_ex.java
+++ app/src/main/java/exercise/mcommerce/gridview/basicGridview_ex.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import exercise.mcommerce.gridview.databinding.ActivityBasicGridviewExBinding;
 
@@ -25,14 +30,26 @@ public class basicGridview_ex extends AppCompatActivity {
 
     private void loadData() {
         String[] data = {"Máy ảnh", "Laptop", "Điện thoại", "Máy tính bảng", "Tai nghe", "Loa", "Ổ cứng di động", "USB", "Thẻ nhớ", "Pin sạc", "Bàn phím", "Chuột"};
+        // convert to modified list
+        List<String> modifiedList = new ArrayList<>(Arrays.asList(data));
         adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                data
+                modifiedList
         );
         binding.gridView.setAdapter(adapter);
     }
 
     private void addEvents() {
+        // Hold to delete
+        binding.gridView.setOnItemLongClickListener((parent, view, position, id) -> {
+            Toast.makeText(
+                    basicGridview_ex.this,
+                    "Xóa " + adapter.getItem(position) + " thành công",
+                    Toast.LENGTH_SHORT).show();
+            adapter.remove(adapter.getItem(position));
+            adapter.notifyDataSetChanged();
+            return true;
+        });
     }
 }
