a5bbf09684017df176e3980212c4d1ed737d282c
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-27 02:59:14 +0700

    Complete recycleView

---
+++ app/src/main/java/exercise/mcommerce/adapters/recyAdapter.java
@@ -0,0 +1,82 @@
+package exercise.mcommerce.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+import exercise.mcommerce.models.Beers;
+import exercise.mcommerce.recycleview_ex.R;
+
+public class recyAdapter extends RecyclerView.Adapter<recyAdapter.ViewHolder> {
+    private List<Beers> list;
+    // Constructor
+    public recyAdapter(List<Beers> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public recyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyAdapter.ViewHolder holder, int position) {
+        Beers beer = list.get(position);
+        holder.beerName.setText(beer.getBeerName());
+        // Convert double to string, add đ to the end and add separator
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.GERMANY);
+        String formattedPrice = numberFormat.format(beer.getBeerPrice()) + " đ";
+        holder.beerPrice.setText(formattedPrice);
+        holder.beerImage.setImageResource(beer.getBeerThumb());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView beerImage;
+        TextView beerName, beerPrice;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            beerImage = itemView.findViewById(R.id.beerImage);
+            beerName = itemView.findViewById(R.id.beerName);
+            beerPrice = itemView.findViewById(R.id.beerPrice);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Do something when the item is clicked
+                    Beers selectedBeer = list.get(getAdapterPosition());
+                    Toast.makeText(itemView.getContext(), selectedBeer.getBeerName() +"-" + selectedBeer.getBeerPrice(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // Do something when the item is held
+                    Beers selectedBeer = list.get(getAdapterPosition());
+                    Toast.makeText(itemView.getContext(), "Deleted " + selectedBeer.getBeerName(), Toast.LENGTH_SHORT).show();
+                    list.remove(selectedBeer);
+                    notifyDataSetChanged();
+                    return true;
+                }
+            });
+        }
+    }
+}
+
--- app/src/main/java/exercise/mcommerce/recycleview_ex/MainActivity.java
+++ app/src/main/java/exercise/mcommerce/recycleview_ex/MainActivity.java
@@ -1,37 +1,81 @@
 package exercise.mcommerce.recycleview_ex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
+import exercise.mcommerce.adapters.recyAdapter;
+import exercise.mcommerce.models.Beers;
 import exercise.mcommerce.recycleview_ex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    ArrayAdapter<String> adapter;
+    recyAdapter adapter;
+
+    ArrayList<Beers> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initData();
         loadData();
         addEventes();
 
     }
 
+    private int currentItem = 0;
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (currentItem == adapter.getItemCount()) {
+                currentItem = 0;
+            }
+            binding.recyclerView.smoothScrollToPosition(currentItem++);
+            handler.postDelayed(this, 2500);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 2500);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+    }
+
+    private void initData() {
+        data = new ArrayList<>();
+        data.add(new Beers(R.drawable.beer333, "Beer 333", 20000.0));
+        data.add(new Beers(R.drawable.hanoi, "Beer Hanoi", 25000.0));
+        data.add(new Beers(R.drawable.saigon, "Beer Saigon", 30000.0));
+        data.add(new Beers(R.drawable.tiger, "Beer Tiger", 35000.0));
+        data.add(new Beers(R.drawable.larue, "Beer Larue", 40000.0));
+        data.add(new Beers(R.drawable.sapporo, "Beer Sapporo", 45000.0));
+        data.add(new Beers(R.drawable.heineken, "Beer Heineken", 50000.0));
+        adapter = new recyAdapter(data);
+    }
+
     private void loadData() {
-        String[] data = {"Máy ảnh", "Laptop", "Điện thoại", "Máy tính bảng", "Tai nghe", "Loa", "Ổ cứng di động", "USB", "Thẻ nhớ", "Pin sạc", "Bàn phím", "Chuột"};
-        adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                data
-        );
-//        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setHasFixedSize(true);
     }
 
     private void addEventes() {
+        // on click show toast of beer name
     }
 }
